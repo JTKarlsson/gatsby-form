a9991370e94ceac0b3aa366999f88347212e701b
Joni karlsson <jonikarlsson@Joni-MBP.home>
2020-09-24 14:29:23 +0300

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+export default function Home() {
+  return (<div>
+    <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
+      <input type="hidden" name="bot-field" />
+      <input type="hidden" name="form-name" value="contact" />
+      <div>
+        <p>Nimi</p>
+        <input type='text' name='nimi' />
+      </div>
+      <div>
+        <p>Sposti</p>
+        <input type='email' name='sposti' />
+      </div>
+      <div>
+        <p>Puhelin</p>
+        <input type='text' name='puhelin' />
+      </div>
+      <div>
+        <p>Viesti</p>
+        <textarea type='text' name='viesti' />
+      </div>
+      <p>Hyväksyn 
+        <input type='checkbox' />
+      </p>
+      <button type='submit'>Lähetä</button>
+    </form>
+  </div>)
+}
