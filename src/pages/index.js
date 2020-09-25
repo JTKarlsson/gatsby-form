af38b77e8b66a7f30096d5c62c1dc5079f3c8a7c
Joni karlsson <jonikarlsson@Joni-MBP.home>
2020-09-25 11:47:37 +0300

    jee

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
+import axios from "axios";
 
 export default function Home() {
-  return (<div>
+
+  return (<div id="contact">
     <form method="post" netlify-honeypot="bot-field" data-netlify="true" 
-    name="contact" action='/'>
+    name="contact" action='https://gatsby-form-joni.netlify.app/#contact'>
       <input type="hidden" name="bot-field" />
       <input type="hidden" name="form-name" value="contact" />
       <div>
