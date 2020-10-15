f903a0a54475d53d24ea3b166bfbaadcbb1761a2
Joni karlsson <jonikarlsson@Joni-MBP.home>
2020-10-15 08:48:46 +0300

    modified index.js..

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,9 @@ import React from "react"
 
 export default function Home() {
 
-  return (<div id="contact2">
+  return (
+  <div>
+  <div id="contact2">
     <form method="post" netlify-honeypot="bot-field" data-netlify="true" 
     name="contact2" >
       <input type="hidden" name="bot-field" />
@@ -29,4 +31,37 @@ export default function Home() {
       <button type='submit'>Lähetä</button>
     </form>
   </div>)
+   <div id="training_contact">
+   <form
+     method="post"
+     netlify-honeypot="bot-field"
+     data-netlify="true"
+     name="training_contact"
+   >
+     <input type="hidden" name="bot-field" />
+     <input type="hidden" name="form-name" value="training_contact" />
+     <div>
+       <p>Nimi</p>
+       <input type="text" name="name" />
+     </div>
+     <div>
+       <p>Sposti</p>
+       <input type="email" name="email" />
+     </div>
+     <div>
+       <p>Puhelin</p>
+       <input type="text" name="tel" />
+     </div>
+     <div>
+       <p>Viesti</p>
+       <textarea type="text" name="message" />
+     </div>
+     <p>
+       Hyväksyn
+       <input type="checkbox" />
+     </p>
+     <button type="submit">Lähetä</button>
+   </form>
+ </div>
+</div>
 }
